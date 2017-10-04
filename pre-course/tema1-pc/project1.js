0b82bf0284b5daa9bba37f5b2e57d2d345a4bd95
Alex <alexbcn@gmail.com>
2017-10-04 19:24:25 +0200

    All in one funtion

--- pre-course/tema1-pc/project1.js
+++ pre-course/tema1-pc/project1.js
@@ -1,59 +1,66 @@
-var num1 = 10;
-var num2 = 25.124 ;
-var results = []
-
-if (num1 === null && num2 === null)
-
-{
-    console.log('Por favor. indique los numeros en las variables del programa.')
-
-}
+calc(10, 10.25);
 
 
-else if (num1 === null || num2 === null)
+function calc (num1, num2)
 {
 
-    if (num1 === null)
+    var results = []
+
+
+
+    if (num1 === null && num2 === null)
+
     {
-        console.log('La raiz cuadrada de ' + num2 + ' es ' + Math.sqrt (num2).toFixed(3))
+        console.log('Por favor. indique los numeros en las variables del programa.')
+
+    }
+
+
+    else if (num1 === null || num2 === null)
+    {
+
+        if (num1 === null)
+        {
+            console.log('La raiz cuadrada de ' + num2 + ' es ' + Math.sqrt (num2).toFixed(3))
+        }
+
+        else
+        {
+            console.log('La raiz cuadrada de ' + num1 + ' es '  + Math.sqrt (num1).toFixed(3))
+        }
+
     }
 
     else
     {
-        console.log('La raiz cuadrada de ' + num1 + ' es '  + Math.sqrt (num1).toFixed(3))
+        calculator (num1, num2);
+
     }
 
-}
 
-else
-{
-    calculator (num1, num2);
+    function calculator (number1, number2)
+    {
+        results.push(number1 + number2);
+        results.push(number1 - number2);
+        results.push(number1 * number2);
+        results.push(number1 / number2);
 
-}
+        return console.log 
 
+        (   
 
-function calculator (number1, number2)
-{
-    results.push(number1 + number2);
-    results.push(number1 - number2);
-    results.push(number1 * number2);
-    results.push(number1 / number2);
-
-    return console.log 
+            'El resultado es:'                            + "\n" +
+            '-------------------------'                   + "\n" +
+            'SUMA:            '   + results[0].toFixed(3) + "\n" +  //toFixed == 3 decilames
+            'RESTA:           '   + results[1].toFixed(3) + "\n" +
+            'MULTIPLICACION:  '   + results[2].toFixed(3) + "\n" +
+            'DIVISION:        '   + results[3].toFixed(3) + "\n" 
         
-    (   
         
-        'El resultado es:'                            + "\n" +
-        '-------------------------'                   + "\n" +
-        'SUMA:            '   + results[0].toFixed(3) + "\n" +  //toFixed == 3 decilames
-        'RESTA:           '   + results[1].toFixed(3) + "\n" +
-        'MULTIPLICACION:  '   + results[2].toFixed(3) + "\n" +
-        'DIVISION:        '   + results[3].toFixed(3) + "\n" 
-    
-    
-    );
-
-    
+        );
 
 
+
+
+    }
 }
