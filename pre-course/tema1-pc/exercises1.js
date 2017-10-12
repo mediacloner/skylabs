b5697c94a7a77dab64c14a9637309ecd19a2b2a0
mediacloner <alexbcn@gmail.com>
2017-10-12 19:50:26 +0200

    Working with exercise of Topic 1

--- pre-course/tema1-pc/exercises1.js
+++ pre-course/tema1-pc/exercises1.js
@@ -1,10 +1,27 @@
 
-var skylab = {
-    lenguaje: 'Javascript',
-    stack: 'MEAN',
-    angular: 4
-}
+// Working space for debbug.
+
+function slashName () {
+    
+        var myName = ['Alex', 'Sanchez']
+
+        var result = '';
+
+        for (var i = 0 ; i < myName.length; i++)
+        {
+            for (var j = 0; j < myName [i].length; j++)
+
+            {
+               result = result + myName [i] [j] + '/'
+            }
 
 
+        }
+        
 
-console.log(skylab.lenguaje)
+        console.log(result);
+       
+        }
+    
+        slashName();
+
