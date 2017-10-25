e6b3e604a75a09e1e8538cb2e0e1a117e30717ac
Alex <alexbcn@gmail.com>
2017-10-25 07:35:04 +0200

    Working "Numbers A)" Exercise

--- pre-course/tema1-pc/exercises1.js
+++ pre-course/tema1-pc/exercises1.js
@@ -1,72 +1,21 @@
-
-// n1) Ahora muestra por consola que letras NO se repiten y muestra tu nombre sin esas letras
-
-
-
-function showMeLetterNoRepeat(name) 
+// a) Que hora es? Declara la hora como número y devuelvela como String.
+function getTime ()
 {
-        strCleanName = cleanName(name);
-        countLetters (strCleanName);
+        var nowDate = new Date();
+        var nowHour = nowDate.getHours();
+        var nowMinute = nowDate.getMinutes();
 
-        function cleanName(strName)
-        {
-                lwcName = strName.toLowerCase();
-                clnName = lwcName.replace(/ /g,''); //remove spaces
-                return clnName;
-        }  
-  
-        function countLetters (strLetters)
+        // Padding 00 like 2:3 = 02:03
+
+        var strPadHour = '1'.padStart(2, "0");
+        var strPadMinute = '2'.padStart(2, "0");
+
+        console.log( 'Hello! It is just ' + strPadHour + ':' + strPadMinute);
+
         
-        {
-                var letters=[];
-                var iteratLetters = [];
-                chargeArrLetters();
-                printRepeatLetters();
-
-
-                function printRepeatLetters ()
-                {
-                        strResult = '';
-                        for (var i = 0; i < letters.length-1; i++) 
-                        {
-                                if (iteratLetters[i]<2)
-                                {
-                                        
-                                        strResult += letters[i];   
-                    
-                                }
-                        }
-
-                        console.log(strResult);
-                }
-                
-
-                function chargeArrLetters()
-                {
-                        for (var i = 0; i < strLetters.length; i++) 
-                        {
-                                if ( letters.indexOf(strLetters[i]) < 0) //insert new letter
-                                {
-                                        letters.push(strLetters[i]);
-                                        iteratLetters.push(1);
-                                }
-
-                                else
-                                {
-                                        var position;
-                                        position = letters.indexOf(strLetters[i]);
-                                        iteratLetters[position]+= 1;
-                                }
-                        }
-
-                }
         
-        }
-
-      
-
-       
 }
-    
-showMeLetterNoRepeat('Alex Sanchez');
+
+getTime();
+
 
