f4eb494a47396d6ef7e0a7eb36ae358e1fa21e18
Alex <alexbcn@gmail.com>
2017-10-26 07:13:49 +0200

    Clean Files structure Files

--- pre-course/Tema2-pc/exercises2.js
+++ pre-course/Tema2-pc/exercises2.js
@@ -1,23 +1,3 @@
-//e) Podemos controlar este error con un condicional if?
-function getCalc (num1, str)
-{
-        var resultMult = num1 * str;
 
-        if (isNaN(resultMult))
-        {
-                resultMult = 'You can\'t do this operation!';
-        }
-
-        console.log( 'Result =>' + resultMult );
-
-
-        
-        
-}
-
-getCalc(2, 'Hola');
-
-
-// Result =>NaN
 
 
