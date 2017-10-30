dbdf679a1abe9a61b8e84df3e9bba9633edc4e22
Alex <alexbcn@gmail.com>
2017-10-30 07:04:00 +0100

    Fix Ex. a) and Done ex. b)

--- pre-course/Tema2-pc/exercises2.js
+++ pre-course/Tema2-pc/exercises2.js
@@ -1,10 +1,23 @@
 // a) Escribe una función que liste los nombres de propiedad 
 //    del objeto (Puedes usar el objeto creado más arriba)
+function showMeObj()
+{
+    var avenger = 
+    { 
+        name : "Tony", 
+        class : "VII", 
+        id : 1 
+    };
 
-var avenger = { 
-    name : "Tony", 
-    class : "VII", 
-    id : 1 
-};
-console.log(avenger.name +', ' +avenger.class +', ' +avenger.id) 
+    for( key in avenger) {
+        // keys
+        console.log(avenger[key]) ;  //key = name of property
+      }
+    
+
+}
+
+showMeObj();
+
+
 
