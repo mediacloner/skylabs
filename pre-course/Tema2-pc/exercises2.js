90d29363d4965083a9f6aa65b37f49667c1010e3
Alex <alexbcn@gmail.com>
2017-10-30 06:35:52 +0100

    Tema 2, Ex a) Done!!!

--- pre-course/Tema2-pc/exercises2.js
+++ pre-course/Tema2-pc/exercises2.js
@@ -1,3 +1,10 @@
+// a) Escribe una función que liste los nombres de propiedad 
+//    del objeto (Puedes usar el objeto creado más arriba)
 
-
+var avenger = { 
+    name : "Tony", 
+    class : "VII", 
+    id : 1 
+};
+console.log(avenger.name +', ' +avenger.class +', ' +avenger.id) 
 
