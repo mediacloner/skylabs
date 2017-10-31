d333eeafa2f686e7c1c7c1f3901ec9c093e403a9
Alex <alexbcn@gmail.com>
2017-10-31 07:07:37 +0100

    Ex. k) Done!!!

--- pre-course/Tema2-pc/exercises2.js
+++ pre-course/Tema2-pc/exercises2.js
@@ -1,13 +1,14 @@
-// j) Crea otro objeto y imprime sus propiedades por pantalla.
+// k) Crea una propiedad del objeto que liste automáticamente los valores de la instancia. 
 
 
 
-function newObjConstructor()
+
+function newFunConstructor()
 {
     
-    var tonyStark = new avenger ("Tony Stark", "XI", "NYC", "Ingeneer", "MIT", 10)
-    var hulk = new avenger ("Robert Bruce Banner", "X", "NYC", "Scientist", "Columbia University", 10)
-    console.log( hulk )  
+    var tonyStark = new avenger ("Tony Stark", "XI", "NYC", "Ingeneer", "MIT", 10);
+    var hulk = new avenger ("Robert Bruce Banner", "X", "NYC", "Scientist", "Columbia University", 10);
+    hulk.description();  // Remember ()
     function avenger (fullName, classRoom, city, job, studies,markAv){
         this.fullName = fullName;
         this.classRoom = classRoom;
@@ -15,11 +16,22 @@ function newObjConstructor()
         this.job= job;
         this.studies= studies;
         this.markAv = markAv;
+        this.description = function () {
+            console.log (
+                          this.fullName 
+                + ' , ' + this.classRoom 
+                + ' , ' + this.city 
+                + ' , ' + this.job
+                + ' , ' + this.studies
+                + ' , ' + this.markAv 
+
+            )
+        }
     }
 
 
     
 }
-newObjConstructor()
+newFunConstructor()
 
 
