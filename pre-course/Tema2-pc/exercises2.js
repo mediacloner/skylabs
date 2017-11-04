8f1b1b8d81ef568aa17dc3d9a9cd6141d916e813
mediacloner <alexbcn@gmail.com>
2017-11-05 00:45:51 +0100

    Ex M) Done!!!

--- pre-course/Tema2-pc/exercises2.js
+++ pre-course/Tema2-pc/exercises2.js
@@ -1,68 +1,66 @@
+
 // m) Crea varios objetos con las mismas propiedades, como por ejemplo la ciudad, crea una función 
 // para que solo liste los nombres de los Avengers que sean de la misma ciudad y cuantos hay.
 
 
 
 
-function listFullName()
-{
+function listFullName() {
     var arrAvengers = [];
     arrAvengers.push(new avenger ('Ironman','Tony Stark', 'XI', 'NYC', 'Ingeneer', 'MIT', 10));
     arrAvengers.push(new avenger ('Hulk','Robert Bruc Banner', 'XI', 'NYC', 'Scientist', 'Columbia University'));
     arrAvengers.push(new avenger('Thor', 'Thor Odinson', 'XI', 'Asgard' , 'Superhero', 'unknown'  ));
     arrAvengers.push(new avenger('Scarlet Witch','Wanda Maximoff', 'X', 'Mount Wundagore', 'Witch', 'unknown'  ));
 
-    findAv ('XI', classRoom)
+    findAv ('XI', 'classRoom')
 
     function findAv(searchDef, keyVar) {
 
-        // a
-        // b
-        // c
-       
-        var a = ['a', 'b', 'c'];
+       var avFiltered= [];
 
-        a.forEach(function (element) {
-            console.log(element);
-        });
+       avFiltered = arrAvengers.filter(function (arrAvengers) { 
+           return arrAvengers[keyVar] == searchDef;
+       });
 
 
+       avFiltered.forEach(function (element) {
 
-        // Filtered array longWords is ["exuberant", "destruction", "present"]
-        var words = ["spray", "limit", "elite", "exuberant", "destruction", "present", "happy"];
-
-        var longWords = words.filter(function (word) {
-            return word.length > 6;
-        });
-
-
-
+           console.log(element.nickname);
+       });
     }
 
+
+
+
+
+
+
+
+
     function avenger (nickname, fullName, classRoom, city, job, studies,markAv){
-        this.nickname = nickname;
-        this.fullName = fullName;
+                    this.nickname = nickname;
+                this.fullName = fullName;
         this.classRoom = classRoom;
         this.city = city;
         this.job= job;
         this.studies= studies;
         this.markAv = markAv;
         this.description = function () {
-            console.log (
-                          this.nickname
-                + ' , ' + this.fullName 
-                + ' , ' + this.classRoom 
-                + ' , ' + this.city 
-                + ' , ' + this.job
-                + ' , ' + this.studies
-                + ' , ' + this.markAv 
+                    console.log(
+                        this.nickname
+                        + ' , ' + this.fullName
+                        + ' , ' + this.classRoom
+                        + ' , ' + this.city
+                        + ' , ' + this.job
+                        + ' , ' + this.studies
+                        + ' , ' + this.markAv
 
-            )
-        }
-    }
+                    )
+                }
+                }
 
 
-    
+
 }
 listFullName();
 
