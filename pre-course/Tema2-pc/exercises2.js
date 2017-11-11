ca1c4ae5e5be46689d7547a7248c374dd74874bc
Alex <alexbcn@gmail.com>
2017-11-11 13:53:45 +0100

    Working with Ex. ñ)

--- pre-course/Tema2-pc/exercises2.js
+++ pre-course/Tema2-pc/exercises2.js
@@ -1,37 +1,32 @@
 
 
-//n) Para acabar, créate a ti mismo y crea una función que recoja todas las markAv y
-// muestre la media.
+// ñ) Ahora, crea una funcion que recoja los avengers en parejas (será necesario que tengan un id,
+// por comodidad al aparejarlos), es decir, de dos en dos, compare sus markAv y que muestre 
+// el mayor de ambos.
 
 
-
-function avgMarkAv() {
+function pairMaxMarkAv() {
     var arrAvengers = [];
-    arrAvengers.push(new avenger ('Ironman','Tony Stark', 'XI', 'NYC', 'Ingeneer', 'MIT', 10));
-    arrAvengers.push(new avenger ('Hulk','Robert Bruc Banner', 'XI', 'NYC', 'Scientist', 'Columbia University',8));
-    arrAvengers.push(new avenger('Thor', 'Thor Odinson', 'XI', 'Asgard' , 'Superhero', 'unknown', 4  ));
-    arrAvengers.push(new avenger('Scarlet Witch','Wanda Maximoff', 'X', 'Mount Wundagore', 'Witch', 'unknown',9  ));
-    arrAvengers.push(new avenger('Buglex', 'Alex Sanchez', 'V', 'Vilanova', 'Dev', 'Life University', 3));
-
-    console.log('Average Mark Avengers is: ' + avgMarkAv())
+    arrAvengers.push(new avenger('1','Ironman','Tony Stark', 'XI', 'NYC', 'Ingeneer', 'MIT', 10));
+    arrAvengers.push(new avenger('2','Hulk','Robert Bruc Banner', 'XI', 'NYC', 'Scientist', 'Columbia University',8));
+    arrAvengers.push(new avenger('3','Thor', 'Thor Odinson', 'XI', 'Asgard' , 'Superhero', 'unknown', 4  ));
+    arrAvengers.push(new avenger('4','Scarlet Witch','Wanda Maximoff', 'X', 'Mount Wundagore', 'Witch', 'unknown',9  ));
+    arrAvengers.push(new avenger('5','Buglex', 'Alex Sanchez', 'V', 'Vilanova', 'Dev', 'Life University', 3));
 
 
-    function avgMarkAv() {
-        var sumMarkAvgrs = 0;
-        var iterationsAvgrs = 0;
 
-        arrAvengers.forEach(function (element) {
-            sumMarkAvgrs = sumMarkAvgrs + element.markAv;
-            iterationsAvgrs ++;
-        }); 
-        
-        return avgMark = sumMarkAvgrs / iterationsAvgrs;
-    }
+
+    function pairMaxArray() {
+        for (i = 0; i <= arrAvengers.length; i= i+2){
+            console.log(arrAvengers.nickname + ' - ' + arrAvengers.nickname )
+        }
+    };
     
 
     function avenger (nickname, fullName, classRoom, city, job, studies,markAv){
-                    this.nickname = nickname;
-                this.fullName = fullName;
+        this.id = id;
+        this.nickname = nickname;
+        this.fullName = fullName;
         this.classRoom = classRoom;
         this.city = city;
         this.job= job;
