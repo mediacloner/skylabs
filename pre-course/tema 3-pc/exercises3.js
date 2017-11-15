d99cb536c4ac5da580955a84c1455d6e8a28c458
Alex <alexbcn@gmail.com>
2017-11-15 19:06:10 +0100

    Ex a-e Project 3 Done!!!

--- pre-course/tema 3-pc/exercises3.js
+++ pre-course/tema 3-pc/exercises3.js
@@ -1,7 +1,23 @@
 
 
-// a) Primero, creamos una función que nos cree un saludo, pasa tu nombre como parámetro y devuélvelo por la consola.
+//e1) Intenta sumarle al resultado de tu segunda funcion, 
+//un numero random del 0-10 y conviertelo todo a un solo string.
 
+var myfuntion = myName()+' ' + (myAge() + myRandomNumber()).toString()
 
+console.log (myfuntion);
 
+function myName() {
+    return 'Alex';
+}
 
+function myAge() {
+    return 41;
+}
+
+function myRandomNumber() {
+
+    return Math.floor((Math.random() * 10) + 1);
+}
+
+//output: IronMan 45
