539fd43c49c0fb624bb2606f17ee3a154c56b973
mediacloner <alexbcn@gmail.com>
2017-11-28 18:20:50 +0100

    a2) Done!!!

--- pre-course/Tema 4 - pc/exercises4.js
+++ pre-course/Tema 4 - pc/exercises4.js
@@ -1,23 +1,34 @@
-// a1) La funcion debería aceptar la array a tratar como argumento.
+// a2) Pasa también el numero a multiplicar a la función como argumento
 
-function showNums() {
+function argnts() {
     var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     var countIter = 0;
-    for (var i = 0; i < nums.length - 1; i++) {
-        var arrSelector = [];
-        var doubles = nums.map(function (x) {
-            return x * 2;
-        });
-        arrSelector.push(doubles[i]);
-        arrSelector.push(doubles[i + 1]);
-        countIter++
 
+    var l = nums.length;
 
-        console.log(countIter + 'ª pareja ' + arrSelector.join('-'))
+    showNums(nums);
+
+    function showNums() {
+        var arrArg = arguments[0];
+        var len = arrArg.length;
+
+        for (var i = 0; i < arrArg.length - 1; i++) {
+
+            var arrSelector = [];
+
+            var doubles = arrArg.map(function (x) {
+                return x * 2;
+            });
+
+            arrSelector.push(doubles[i]);
+            arrSelector.push(doubles[i + 1]);
+            countIter++
+
+            console.log(countIter + 'ª pareja ' + arrSelector.join('-'))
+        }
 
     }
 }
 
-showNums();
 
 
