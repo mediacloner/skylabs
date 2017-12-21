deb8ca3c75604e2deed51427304d62a2a67883ef
mediacloner <alexbcn@gmail.com>
2017-12-21 22:00:04 +0100

    Working...

---
+++ pre-course/Tema 7 - pc/js.js
@@ -0,0 +1,14 @@
+function changeYellow(elementId) {
+    document.getElementById(elementId).className = "round yellow";
+    
+}
+function changeRed(elementId) {
+    document.getElementById(elementId).className = "round red";
+
+}
+
+function changeGreen(elementId) {
+    document.getElementById(elementId).className = 'round green';
+
+}
+
