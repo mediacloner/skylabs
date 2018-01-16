b36418b6340dba24ff4e0135973081b3a031c0ca
mediacloner <alexbcn@gmail.com>
2018-01-16 23:00:22 +0100

    Execise done!

--- course/topics/objects/scripts/main.js
+++ course/topics/objects/scripts/main.js
@@ -6,5 +6,20 @@ function isObject(input) {
 }
 
 var trueObj = {};
+var falseObj = 'false';
 
-console.log(isObject (trueObj))
+console.log(isObject (trueObj));
+console.log(isObject(falseObj));
+
+function msgCar(objCar) {
+
+  return "My " + objCar.color + " " + objCar.brand + " " + objCar.model + " is great!";
+  
+}
+
+
+var car = { brand: "Seat", model: "Leon", color: "black" };
+
+console.log(msgCar(car))
+
+
