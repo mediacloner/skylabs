1104636b6f6dd3f111a480e000db583882bb2334
mediacloner <alexbcn@gmail.com>
2018-01-19 15:22:33 +0100

    new topic jasmine

---
+++ course/topics/jasmineTest/scripts/main.js
@@ -0,0 +1,20 @@
+//Array.prototype.random() to randomly disorder an array into a new array.
+
+Array.prototype.random = function() {
+  var self = this;
+  var lenName = self.length;
+  var newArray = [];
+
+  for (var i = 0; i < lenName; i++) {
+    randomize();
+  }
+
+  return newArray;
+
+  function randomize() {
+    var x = Math.floor(Math.random() * self.length);
+
+    newArray.push(self[x]);
+    self.splice(x, 1);
+  }
+};
