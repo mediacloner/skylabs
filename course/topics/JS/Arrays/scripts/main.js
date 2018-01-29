beebecdf241cb3e52e958b946ef7e02e311eb71c
mediacloner <alexbcn@gmail.com>
2018-01-29 08:27:14 +0100

    Add new done exercise. Leap Years on Arrays

--- course/topics/JS/Arrays/scripts/main.js
+++ course/topics/JS/Arrays/scripts/main.js
@@ -365,3 +365,19 @@ showChoices(col);
 //
 //Find the leap years in a given range of years.
 
+function showLeapYears(yearIni, yearEnd){
+  years = yearEnd - yearIni;  
+      for (var i = 0; i<= years; i++){
+        var y = yearIni + i;
+          if (isLeap (y))  console.log ('Leap year: ' + y)
+      }
+
+    function isLeap(year) {
+
+      if (year % 400 === 0) return true;
+      else if (year % 4 === 0 && year % 100 != 0 ) return true;
+      else return false;
+      
+    }
+
+}
