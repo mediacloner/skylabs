a52c375725177249ecdb5c1daa2c41e30a70bad4
mediacloner <alexbcn@gmail.com>
2018-02-05 09:36:06 +0100

    Add new exercise

---
+++ course/topics/JS/ATM/scripts/main.js
@@ -0,0 +1,24 @@
+/*
+Write a program that accepts a string and outputs
+the smallest number of coins needed to make change,
+using only quarters, dimes, nickels, and pennies.
+
+Example:
+input: 1.05
+output: "4 Quarters, 1 Nickel"
+*/
+"use strict"
+
+function getCash$(value) {
+coins [100, 25, 10, 5, 1];
+coinName ['dollar', 'quarter', 'dime', 'nickels', 'penny'];
+value 
+
+return cash;
+}
+
+NOTE: coin values are:
+- quarter -> 25 cents
+- dime -> 10 cents
+- nickels -> 5 cents
+- penny -> 1 cent
