bef4fb672f39ab87bba6719713e3e446d314a865
mediacloner <alexbcn@gmail.com>
2018-02-20 00:21:31 +0100

    update of the day

---
+++ course/docs/node.md
@@ -0,0 +1,17 @@
+#Node
+
+
+If you write node you can acces to write js like console of browser.
+
+
+Global is like a window in the browser.
+
+Process
+
+
+
+#DevTool
+
+
+sudo npm i -g devtool
+
---
+++ course/topics/backend/node/learnyounode/program4.js
@@ -0,0 +1,54 @@
+// const fs = require("fs");
+
+// const path = process.argv[2]
+
+// function readFile (path, encoding, callback){
+//     fs.readFile (path, 'utf-8', (err, data) => {
+//         if (err) throw new Error ('ERR-IO-ASYNC')
+
+//         callback (data)
+
+//     })
+
+// }
+
+// try {
+//     readFile (path, 'utf-8', data => console.log (data))
+// } catch (readFile) {
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fs.readFile (path, 'utf-8', (err,data) =>{
+    if (err) throw)
+
+})
+
+
+function callback(err, pFile) {
+    let rawFile =  fs.readFile(pFile);
+
+    console.alert(err)
+}
+
+r
+let arr = rawFile.toString().split("\n");
+
+console.log (arr.length-1)
