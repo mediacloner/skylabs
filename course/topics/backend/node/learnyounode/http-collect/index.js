aeefd053b227c06802c45838f8ab9e83e4aafe5a
mediacloner <alexbcn@gmail.com>
2018-02-20 10:14:31 +0100

    new exercise of node

---
+++ course/topics/backend/node/learnyounode/http-collect/index.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const url = process.argv[2]
+
+http.get (url, res => {
+
+    res.setEncoding ('utf-8')
+    let rawData = ''
+
+    res.on ('data', (chunk) => {
+
+        rawData += chunk
+    })
+
+    res.on ('error', console.error)
+
+
+    res.on ('end', () => {
+        console.log(rawData.length)
+        console.log(rawData)
+
+
+
+    })
+
+})
