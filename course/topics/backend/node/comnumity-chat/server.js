1c64d6a1d6ca15fa095e00e980f739c01a0276b2
mediacloner <alexbcn@gmail.com>
2018-02-21 21:50:09 +0100

    update node

--- course/topics/backend/node/comnumity-chat/server.js
+++ course/topics/backend/node/comnumity-chat/server.js
@@ -16,7 +16,7 @@ var net = require('net');
 var client = new net.Socket();
 client.connect(3000, '192.168.0.16', function() {
 	console.log('Connected');
-    client.write(`ACK ACK ACK !!!!`);
+    client.write(`Es una broma! ACK ACK ACK !!!!`);
 });
 
 client.on('data', data => console.log(data.toString()))
