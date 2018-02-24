667947d2a8bf63e04a29a37a33b0a6420527e675
mediacloner <alexbcn@gmail.com>
2018-02-25 00:04:18 +0100

    New structure for  version Pro

---
+++ course/topics/backend/express/task-API-pro/index.js
@@ -0,0 +1,177 @@
+require("dotenv").config();
+
+const _ = require("lodash");
+const bodyParser = require("body-parser");
+const dataPath = process.env.DATA_PATH;
+
+const express = require("express");
+
+const app = express();
+
+let tasks = [];
+let id = 0;
+
+// create task
+
+const jsonBodyParser = bodyParser.json();
+
+
+app.post("/api/tasks", jsonBodyParser, (req, res) => {
+  const { text, username } = req.body;
+  id +=1;
+  const task = {
+    username ,
+    id,
+    done: false,
+    text 
+  };
+
+  tasks.push(task)
+    ? res.json(OKConcatResult("Task Add!", task))
+    : NOKConcatResult(
+        "Somenthing wrong happens!",
+        "We can not include this task"
+      );
+});
+
+//list done
+app.get("/api/tasks/done", (req, res) => {
+
+  let result=[]
+  tasks.forEach(
+    ({ username, id, text, done }) => {
+      
+      if (done) result.push( { username, id, text, done } )
+    })
+
+  res.json(
+    OKConcatResult(
+      "List of done tasks.", result
+      )
+    )
+  
+});
+
+
+
+// list todo
+app.get("/api/tasks/todo", (req, res) => {
+  let result=[]
+  tasks.forEach(
+    ({ username, id, text, done }) => {
+      
+      if (!done) result.push( { username, id, text, done } )
+    })
+
+  res.json(
+    OKConcatResult(
+      "List of to do tasks.",
+      result
+    )
+  );
+});
+
+// Mark done
+
+app.put("/api/tasks/:id", (req, res) => {
+
+
+  const idInt = parseInt( req.params.id)
+
+  const pos = _.findIndex(tasks, (o) => { return o.id == idInt; });
+  const result = tasks[pos]
+
+  if (result === undefined) {
+  res.json(
+      NOKConcatResult(
+        "Error",
+        "The Id not exist"
+      ))
+ }
+ else {
+  tasks[pos].done = true
+
+  res.json(
+      OKConcatResult(
+        "The task is done!.",
+        result
+      ))
+ }
+
+  
+  
+});
+
+//remove all
+
+app.delete("/api/tasks", (req, res) => {
+  let result=[]
+  tasks.forEach(
+    ({ username, id, text, done }) => {
+      
+     result.push( { username, id, text, done } )
+    })
+
+
+    tasks = []
+
+  res.json(
+    OKConcatResult(
+      "Delete all tasks.", result
+      )
+    )
+ 
+
+
+});
+
+// remove by id
+
+app.delete("/api/tasks/:id", (req, res) => {
+
+  const idInt = parseInt( req.params.id)
+
+  const pos = _.findIndex(tasks, (o) => { return o.id == idInt; });
+  const result = tasks[pos]
+
+  if (result === undefined) {
+  res.json(
+      NOKConcatResult(
+        "Error",
+        "The Id not exist"
+      ))
+ }
+ else {
+  tasks.splice(pos,1)
+
+  res.json(
+      OKConcatResult(
+        "Delete task.",
+        result
+      ))
+ }
+
+  
+});
+
+// update task
+
+app.patch("/api/tasks/:id", jsonBodyParser, (req, res) => {});
+
+function OKConcatResult(message, data) {
+  const res = { status: "OK", message };
+
+  if (data) res.data = data;
+
+  return res;
+}
+
+function NOKConcatResult(message, error) {
+  const res = { status: "KO", message };
+
+  if (error) res.error = error;
+
+  return res;
+}
+const port = process.env.PORT;
+app.listen(port, () => console.log(`We are listen in the port ${port}`));
---
+++ course/topics/backend/express/user-API/index.params.js
@@ -0,0 +1,87 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const users = []
+
+app.get('/api/users', (req, res) => res.json(ok('Users listing succeeded.', users.map(({ username,password }) => ({ username, password })))))
+
+const jsonBodyParser = bodyParser.json()
+
+app.post('/api/users', jsonBodyParser, (req, res) => {
+    const { username, password } = req.body
+
+    if (!username || !password) return res.json(ko('User registration failed.', 'Invalid username and/or password.'))
+
+    const alreadyExists = users.some(user => user.username === username)
+
+    if (alreadyExists) return res.json(ko('User registration failed.', 'Username already exists.'))
+
+    users.push({ username, password })
+
+    res.json(ok('User registration succeeded.'))
+})
+
+
+app.put('/api/users', jsonBodyParser, (req, res) => {
+    const { username, password } = req.body
+
+    function changePass(obj, user, pass ) {
+        for (var i in obj) {
+          if (obj[i].username == user) {  /// user
+             obj[i].password = pass
+             break; //Stop this loop, we found it!
+          }
+        }
+     }
+
+    if (!username || !password) return res.json(ko('User update action failed.', 'Invalid username and/or password.'))
+
+        
+    const alreadyExists = users.some(user => user.username === username)
+
+    changePass (users, username, password)
+
+    res.json(ok('User password update succeeded.'))
+})
+
+app.delete('/api/users', jsonBodyParser, (req, res) => {
+    const { username, password } = req.body
+
+    if (!username || !password) return res.json(ko('User delete action failed.', 'Invalid username and/or password.'))
+
+    const alreadyExists = users.some(user => user.username === username)
+
+
+    //user.forEach ((_user, i )) users.splice(i, 1)
+    users.pop({ username, password })
+
+    res.json(ok('User deleted succeeded.'))
+})
+
+
+
+
+function ok(message, data) {
+    const res = { status: 'OK', message }
+
+    if (data) res.data = data
+
+    return res
+}
+
+function ko(message, error) {
+    const res = { status: 'KO', message }
+
+    if (error) res.error = error
+
+    return res
+}
+
+const port = process.env.PORT
+
+app.listen(port, () => console.log(`Users API running on port ${port}`))
